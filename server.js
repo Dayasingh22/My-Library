51c00e58ae35eb7f0cfa1253b6b1b0bb4ee766ea
Dayasingh22 <dayanidhi.singh.sd@mmumullana.org>
2020-03-25 16:36:17 -0700

    Books Rote

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -29,6 +32,7 @@ db.once('open', () => console.log("Connected to Mongoose"))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 
 app.listen(process.env.PORT || 3000)
